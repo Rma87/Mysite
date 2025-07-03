3b4629e02b47be6d9293a7302d37365692a93f9c
Rastin Rostami zadeh <rrastin633@gmail.com>
2025-07-03 20:59:51 +0330

    change admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display = ('title', 'status', 'id', 'created_date', 'updated_date')
+    search_fields = ('title', 'content')
+    list_filter = ('status', 'published_date')
