e107e8e1ec1f96d759694beffcaa1bcde1567ecf
Rastin Rostami zadeh <165579071+Rma87@users.noreply.github.com>
2025-08-08 19:02:04 +0330

    change test

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from blog.models import *
+from django.shortcuts import render,get_object_or_404
+from blog.models import Post
+
 def blog_view(request):
     posts = Post.objects.filter(status = 1)
     context = {'posts': posts}
@@ -8,7 +9,8 @@ def blog_view(request):
 def blog_single(request):
     return render(request,'blog\\blog-single.html')
 
-def test(request):
-    posts = Post.objects.all()
-    context = {'posts': posts}
-    return render(request, 'test.html', context)
+def test(request,pid):
+    #post = Post.objects.get(id = pid)
+    post = get_object_or_404(Post, id=pid)
+    context = {'post': post}
+    return render(request,'test.html',context)
