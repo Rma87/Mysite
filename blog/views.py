7f76fa6a014402cd8c30ac4129124f288eeab995
Rastin Rostami zadeh <165579071+Rma87@users.noreply.github.com>
2025-08-28 22:44:36 +0330

    fix

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,12 @@ from blog.models import Post
 def blog_view(request):
     posts = Post.objects.filter(status = 1)
     context = {'posts': posts}
-    return render(request,'blog\\blog-home.html', context)
+    return render(request,'blog//blog-home.html', context)
     
-def blog_single(request):
-    return render(request,'blog\\blog-single.html')
+def blog_single(request,pid):
+    post = get_object_or_404(Post, id=pid, status=1)
+    context = {'post': post}
+    return render(request,'blog//blog-single.html',context)
 
 def test(request,pid):
     #post = Post.objects.get(id = pid)
