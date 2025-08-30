8f2282ff87d00d1bad5e732d26a485643a1fe662
Rastin Rostami zadeh <165579071+Rma87@users.noreply.github.com>
2025-08-30 13:05:45 +0330

    add image
    +image field
    +install pillow

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 class Post(models.Model):
-    # image =
+    image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)#  models.CASCADE
     title = models.CharField(max_length=255)
     content = models.TextField()
