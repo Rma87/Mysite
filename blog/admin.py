22e99ef931e84d215861e79723be836ba4f7ff3f
Rastin Rostami zadeh <165579071+Rma87@users.noreply.github.com>
2025-08-31 19:12:56 +0330

    Add category field

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post,Category
 
 # Register your models here.
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_filter = ('name',)
+    search_fields = ('name',)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -10,3 +14,4 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title','author', 'status', 'id', 'created_date', 'updated_date')
     search_fields = ('title', 'author')
     list_filter = ('status', 'published_date')
+
